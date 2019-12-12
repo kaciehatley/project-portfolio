0aaaf17fb074dd63ec497b44e056b09be50edd77
Kacie Hatley <kaciehatley@gmail.com>
2019-12-12 14:46:51 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var simpleEatsBlock = document.querySelector(".simpleEats");
+var jsQuizBlock = document.querySelector(".jsQuiz");
+var weatherDashBlock = document.querySelector(".weatherDash");
+var pwGenBlock = document.querySelector(".pwGen");
+var dayPlanBlock = document.querySelector(".dayPlan");
+
+simpleEatsBlock.addEventListener("click", function() {
+    window.open( 
+        "https://kaciehatley.github.io/simple-eats/index2.html", "_blank"); 
+});
+
+jsQuizBlock.addEventListener("click", function() {
+    window.open( 
+        "https://kaciehatley.github.io/code-quiz/", "_blank"); 
+});
+
+weatherDashBlock.addEventListener("click", function() {
+    window.open( 
+        "https://kaciehatley.github.io/weather-dashboard/", "_blank"); 
+});
+
+pwGenBlock.addEventListener("click", function() {
+    window.open( 
+        "https://kaciehatley.github.io/password_generator/", "_blank"); 
+});
+
+dayPlanBlock.addEventListener("click", function() {
+    window.open( 
+        "https://kaciehatley.github.io/day-planner/", "_blank"); 
+});
+
+
