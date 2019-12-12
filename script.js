e80d7dd6b015c2d581253ada0e553c4d4efe5719
Kacie Hatley <kaciehatley@gmail.com>
2019-12-12 15:40:15 -0500

    added contact section

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var jsQuizBlock = document.querySelector(".jsQuiz");
 var weatherDashBlock = document.querySelector(".weatherDash");
 var pwGenBlock = document.querySelector(".pwGen");
 var dayPlanBlock = document.querySelector(".dayPlan");
+var gitHub = document.querySelector("#gitHub");
+var linkedIn = document.querySelector("#linkedIn");
 
 simpleEatsBlock.addEventListener("click", function() {
     window.open( 
@@ -29,4 +31,13 @@ dayPlanBlock.addEventListener("click", function() {
         "https://kaciehatley.github.io/day-planner/", "_blank"); 
 });
 
+gitHub.addEventListener("click", function() {
+    window.open( 
+        "https://github.com/kaciehatley", "_blank"); 
+});
+
+linkedIn.addEventListener("click", function() {
+    window.open( 
+        "https://www.linkedin.com/in/kacie-hatley-a1392518a", "_blank"); 
+});
 
